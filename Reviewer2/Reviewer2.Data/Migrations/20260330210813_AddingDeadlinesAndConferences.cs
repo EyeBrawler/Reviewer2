940643acfa639acd4acbef34deb16d788083627d
Sam Thyen <Eye_Brawler@outlook.com>
2026-03-30 16:11:03 -0500

    Database Migration for Conferences and Deadlines

---
+++ Reviewer2/Reviewer2.Data/Migrations/20260330210813_AddingDeadlinesAndConferences.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
+
+#nullable disable
+
+namespace Reviewer2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddingDeadlinesAndConferences : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "conferences",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    name = table.Column<string>(type: "text", nullable: true),
+                    description = table.Column<string>(type: "text", nullable: true),
+                    call_for_papers = table.Column<string>(type: "text", nullable: true),
+                    json_data = table.Column<string>(type: "text", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_conferences", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "deadlines",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "integer", nullable: false)
+                        .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
+                    conference_id = table.Column<int>(type: "integer", nullable: false),
+                    name = table.Column<string>(type: "text", nullable: false),
+                    date = table.Column<DateTimeOffset>(type: "timestamp with time zone", nullable: false),
+                    priority = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_deadlines", x => x.id);
+                    table.ForeignKey(
+                        name: "fk_deadlines_conferences_conference_id",
+                        column: x => x.conference_id,
+                        principalTable: "conferences",
+                        principalColumn: "id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_deadlines_conference_id",
+                table: "deadlines",
+                column: "conference_id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "deadlines");
+
+            migrationBuilder.DropTable(
+                name: "conferences");
+        }
+    }
+}
