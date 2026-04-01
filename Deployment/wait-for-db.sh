a0c9d3ffdf41c00fd8e1bb05a88165dbe3b9a36b
Sam Thyen <Eye_Brawler@outlook.com>
2026-03-31 23:10:24 -0500

    Simplifying wait-for-db.sh

--- Deployment/wait-for-db.sh
+++ Deployment/wait-for-db.sh
@@ -3,15 +3,11 @@ set -e
 
 echo "Waiting for database at $ConnectionStrings__Reviewer2Connection..."
 
-cd /app/Reviewer2
-
-dotnet restore
-
 # Move into the Data project directory
 cd /app/Reviewer2/Reviewer2.Data
 
 # Retry until DB is ready and migrations succeed
-until dotnet ef database update --startup-project ../Reviewer2.Blazor; do
+until dotnet ef database update; do
   echo "Database not ready yet, retrying in 5 seconds..."
   sleep 5
 done
