81723f7686e9583029bca0e9805b6ec5a3fd1f63
Sam Thyen <Eye_Brawler@outlook.com>
2026-04-16 21:39:52 -0500

    Database Migration for Accepting Papers only with Revisions (renmaing comment and time columns to fit the enum change in the model).

---
+++ Reviewer2/Reviewer2.Data/Migrations/20260417023748_AcceptWithRevisionsForPapers.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Reviewer2.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AcceptWithRevisionsForPapers : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "decision_made_at_utc",
+                table: "papers",
+                newName: "last_decision_at_utc");
+
+            migrationBuilder.RenameColumn(
+                name: "decision_comment",
+                table: "papers",
+                newName: "decision_or_revision_comment");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "last_decision_at_utc",
+                table: "papers",
+                newName: "decision_made_at_utc");
+
+            migrationBuilder.RenameColumn(
+                name: "decision_or_revision_comment",
+                table: "papers",
+                newName: "decision_comment");
+        }
+    }
+}
